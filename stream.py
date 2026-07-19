1c105ee95389ea45d51925e38bfa6ce32155e6f1
aouiouat <115593822+aouiouatislem@users.noreply.github.com>
2026-07-19 16:36:47 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,14 +1,26 @@
 from flask import redirect
 import database
 
-def play(stream_id):
+def play(stream_id, stream_type):
     try:
         stream_id = int(stream_id)
     except:
-        return "Invalid Stream", 404
+        return "Invalid Stream ID", 404
 
-    for ch in database.channels:
-        if ch["id"] == stream_id:
-            return redirect(ch["url"], code=302)
+    if stream_type == "live":
+        for ch in database.lives:
+            if ch["stream_id"] == stream_id:
+                return redirect(ch["direct_source"], code=302)
+                
+    elif stream_type == "movie":
+        for m in database.movies:
+            if m["stream_id"] == stream_id:
+                return redirect(m["direct_source"], code=302)
+                
+    elif stream_type == "series":
+        for ep_list in database.series_episodes.values():
+            for ep in ep_list:
+                if ep["id"] == stream_id:
+                    return redirect(ep["direct_source"], code=302)
 
     return "Stream Not Found", 404
