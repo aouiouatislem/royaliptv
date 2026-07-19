1ecab9ec212bcb80ed805120ee731a3277bb690d
aouiouat <115593822+aouiouatislem@users.noreply.github.com>
2026-07-19 16:36:14 +0100

    Update xtream.py

--- xtream.py
+++ xtream.py
@@ -1,53 +1,80 @@
-from flask import jsonify
+from flask import jsonify, request
 import database
 
 def live_categories():
     return jsonify([
-        {
-            "category_id": str(cid),
-            "category_name": name,
-            "parent_id": 0
-        }
-        for name, cid in database.categories.items()
+        {"category_id": str(cid), "category_name": name, "parent_id": 0}
+        for name, cid in database.live_categories.items()
     ])
 
-
 def live_streams():
-    data = []
+    category_id = request.args.get("category_id")
+    if category_id:
+        return jsonify([ch for ch in database.lives if ch["category_id"] == str(category_id)])
+    return jsonify(database.lives)
 
-    for ch in database.channels:
-        data.append({
-            "num": ch["id"],
-            "name": ch["name"],
-            "stream_type": "live",
-            "stream_id": ch["id"],
-            "stream_icon": "",
-            "epg_channel_id": "",
-            "added": ch.get("added", "1600000000"),
-            "category_id": str(ch["category_id"]),
-            "custom_sid": "",
-            "tv_archive": 0,
-            "direct_source": ch["url"],
-            "tv_archive_duration": 0
-        })
-
-    return jsonify(data)
-
-# دوال لتجنب Crash عند محاولة التطبيق جلب VOD و Series
 def vod_categories():
-    return jsonify([])
+    return jsonify([
+        {"category_id": str(cid), "category_name": name, "parent_id": 0}
+        for name, cid in database.vod_categories.items()
+    ])
 
 def vod_streams():
-    return jsonify([])
+    category_id = request.args.get("category_id")
+    if category_id:
+        return jsonify([m for m in database.movies if m["category_id"] == str(category_id)])
+    return jsonify(database.movies)
 
 def series_categories():
-    return jsonify([])
+    return jsonify([
+        {"category_id": str(cid), "category_name": name, "parent_id": 0}
+        for name, cid in database.series_categories.items()
+    ])
 
 def series():
-    return jsonify([])
+    category_id = request.args.get("category_id")
+    if category_id:
+        return jsonify([s for s in database.series_list if s["category_id"] == str(category_id)])
+    return jsonify(database.series_list)
 
 def series_info():
-    return jsonify({})
+    try:
+        sid = int(request.args.get("series_id", 0))
+    except:
+        return jsonify({})
+        
+    episodes = database.series_episodes.get(sid, [])
+    
+    seasons_dict = {}
+    for ep in episodes:
+        s_num = str(ep["season"])
+        if s_num not in seasons_dict:
+            seasons_dict[s_num] = []
+        seasons_dict[s_num].append(ep)
+        
+    info = {}
+    for s in database.series_list:
+        if s["series_id"] == sid:
+            info = {
+                "name": s["name"],
+                "cover": s["cover"],
+                "plot": s["plot"],
+                "cast": s["cast"],
+                "director": s["director"],
+                "genre": s["genre"],
+                "releaseDate": s["releaseDate"],
+                "rating": s["rating"]
+            }
+            break
+
+    return jsonify({
+        "seasons": [
+            {"air_date": "", "episode_count": len(v), "id": int(k), "name": f"Season {k}", "overview": "", "season_number": int(k)}
+            for k, v in seasons_dict.items()
+        ],
+        "info": info,
+        "episodes": seasons_dict
+    })
 
 def short_epg():
     return jsonify({"epg_listings": []})
@@ -59,15 +86,10 @@ def vod_info():
     return jsonify({"info": {}})
 
 def simple_data_table():
-    # بعض التطبيقات تطلب هذا الـ action لجلب كل شيء دفعة واحدة
     return jsonify({
         "categories": [
-            {
-                "category_id": str(cid),
-                "category_name": name,
-                "parent_id": 0
-            }
-            for name, cid in database.categories.items()
+            {"category_id": str(cid), "category_name": name, "parent_id": 0}
+            for name, cid in database.live_categories.items()
         ],
         "streams": []
     })
