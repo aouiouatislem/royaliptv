5c64e8d8af1cbd4eee35d4834eb2d2709476c696
aouiouat <115593822+aouiouatislem@users.noreply.github.com>
2026-07-19 15:16:33 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,8 @@
 from flask import jsonify
-
-USERNAME = "royal"
-PASSWORD = "123"
+from config import *
 
 def login(username, password):
+
     if username != USERNAME or password != PASSWORD:
         return jsonify({
             "user_info": {
@@ -18,13 +17,17 @@ def login(username, password):
             "status": "Active",
             "username": USERNAME,
             "password": PASSWORD,
-            "max_connections": 1,
-            "active_cons": 0
+            "max_connections": MAX_CONNECTIONS,
+            "active_cons": 0,
+            "allowed_output_formats": [
+                "ts",
+                "m3u8"
+            ]
         },
         "server_info": {
-            "url": "royaliptv.onrender.com",
-            "https_port": "443",
+            "url": HOST.replace("https://", ""),
             "server_protocol": "https",
-            "timezone": "UTC"
+            "https_port": "443",
+            "timezone": TIMEZONE
         }
     })
